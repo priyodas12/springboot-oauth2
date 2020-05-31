48a74c072a6f47c68245fd5bfb8510887a73717e
Priyobrato Das <priyodas12@gmail.com>
2020-05-31 23:54:54 +0530

    end point added

--- springboot-oauth2/src/main/java/io/springlab/springbootoauth2/SpringbootOauth2Application.java
+++ springboot-oauth2/src/main/java/io/springlab/springbootoauth2/SpringbootOauth2Application.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-
+/*
+*localhost:9992/oauth/token
+*localhost:9992/oauth/check_token?token=23d3d076-7eb7-4c50-8e6f-3c0f4fb873ca
+*/
 @SpringBootApplication
 @EnableAuthorizationServer
 public class SpringbootOauth2Application {
